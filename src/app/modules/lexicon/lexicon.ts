da1020c8ea30442f96bfe294f4a76a90837d1469
avinav singh <avinav.singh@bostonbyte.com>
2020-02-07 23:12:42 +0530

    rmoved

--- src/app/modules/lexicon/lexicon.ts
+++
@@ -1,9 +0,0 @@
-export class Lexicon {
-  public id: any;
-  public lexicon: String;
-  public tslexicon: String;
-  public weight: Number;
-  public selected: Boolean;
-  public active: Boolean;
-  public uploadFile: any;
-}
--- src/app/modules/nQube/rule/rule-group/rule-group.ts
+++
@@ -1,6 +0,0 @@
-export class RuleGroup {
-  public _id: any;
-  public groupName: String;
-  public organizationid: String;
-  public active: Boolean;
-}
